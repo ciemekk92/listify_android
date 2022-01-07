79cefb97c463baf8db08e10d93e6f320fdec65e5
ciemekk92 <przemyslaw.reducha@gmail.com>
2022-01-07 13:42:19 +0100

    feat/add constants, basic resources and activities

--- app/src/main/java/com/example/listify/activities/AboutActivity.java
+++ app/src/main/java/com/example/listify/activities/AboutActivity.java
@@ -1,14 +1,16 @@
-package com.example.listify;
+package com.example.listify.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.listify.R;
+
+public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_about);
     }
 }
---
+++ app/src/main/java/com/example/listify/fragments/BottomSheetFragment.java
@@ -0,0 +1,25 @@
+package com.example.listify.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.listify.R;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class BottomSheetFragment extends BottomSheetDialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.bottom_sheet, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
